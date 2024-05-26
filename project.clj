532a95cc101d77a49a30ca1d8065637b6c8e4a0f
Ludger Solbach <soulman@soulspace.org>
2024-05-26 12:14:18 +0200

    released version 0.8.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.soulspace.clj/math.financial "0.8.0-SNAPSHOT"
+(defproject org.soulspace.clj/math.financial "0.8.0"
   :description "The math.financial library contains financial math functions in Clojure."
   :url "https://github.com/soulspace-org/math.financial"
   :license {:name "Eclipse Public License"
@@ -9,8 +9,7 @@
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]}
 
-;  :dependencies [[org.clojure/clojure "1.11.1"]
-;                 [org.soulspace.clj/math.core "0.9.0"]]
+;  :dependencies [[org.clojure/clojure "1.11.3"]]
 
   :test-paths ["test"]
   :scm {:name "git" :url "https://github.com/soulspace-org/math.financial"}
